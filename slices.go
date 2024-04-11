21c0e38a685f57be96ecd19f1bb1325a3c9e2d9d
vanvazz23 <166739023+vanvazz23@users.noreply.github.com>
2024-04-11 18:37:15 +0200

    slices.go

--- slices.go
+++ slices.go
@@ -6,7 +6,40 @@ import (
 	"strings"
 )
 
-var emailWriterChan chan string // Channel to send extracted emails to the writer goroutine
+var emailWriterChan chan string
+
+func StartEmailWriter(path string) {
+	emailWriterChan = make(chan string)
+
+	go func() {
+		file, err := os.Create(path)
+		if err != nil {
+			fmt.Println("Error creating file:", err)
+			return
+		}
+		defer file.Close()
+
+		for email := range emailWriterChan {
+			if _, err := file.WriteString(email + "\n"); err != nil {
+				fmt.Println("Error writing to file:", err)
+				return
+			}
+		}
+	}()
+}
+
+// IsEqualSlice checks if two slices of strings are equal.
+func IsEqualSlice(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
 
 // UniqueStrings returns a new slice with unique strings from the given slice.
 func UniqueStrings(input []string) []string {
@@ -17,33 +50,28 @@ func UniqueStrings(input []string) []string {
 		if _, exists := seen[value]; !exists {
 			seen[value] = struct{}{}
 			result = append(result, value)
-
-			// Save the email to the file
-			if emailWriterChan != nil {
-				emailWriterChan <- value
-			}
 		}
 	}
 
 	return result
 }
 
-// StartEmailWriter starts the goroutine to write emails to a file.
-func StartEmailWriter(filePath string) {
-	emailWriterChan = make(chan string)
+// FilterOutCommonExtensions filters out common file extensions from a slice of strings.
+func FilterOutCommonExtensions(input []string) []string {
+	exts := []string{".png", ".jpg", ".jpeg", ".gif", ".css", ".js", ".ico", ".svg", ".webp", ".pdf", ".zip", ".rar", ".tar", ".gz", ".7z", ".mp3", ".mp4", ".avi", ".mkv", ".mov", ".wmv", ".flv", ".m4v", ".webm", ".ogg", ".flac", ".wav", ".aac", ".wma", ".m4a", ".opus", ".mid", ".midi", ".mpg", ".mpeg", ".m4v", ".wmv", ".flv", ".m4v", ".webm", ".ogg", ".flac", ".wav", ".aac", ".wma", ".m4a", ".opus", ".mid", ".midi", ".mpg", ".mpeg"}
 
-	go func() {
-		file, err := os.Create(filePath)
-		if err != nil {
-			panic(err)
-		}
-		defer file.Close()
-
-		for email := range emailWriterChan {
-			_, err := file.WriteString(email + "\n")
-			if err != nil {
-				panic(err)
+	filtered := []string{}
+	for _, file := range input {
+		hasCommonExtension := false
+		for _, ext := range exts {
+			if strings.HasSuffix(strings.ToLower(file), ext) {
+				hasCommonExtension = true
+				break
 			}
 		}
-	}()
+		if !hasCommonExtension {
+			filtered = append(filtered, file)
+		}
+	}
+	return filtered
 }
